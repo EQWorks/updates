cb0f1df17f5e799187bc80369be8935b84f2269d
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-11-27 15:54:24 -0500

    issues - enrich PRs with relevant commits
    - if not closed, commits within search range or all if newly created PR;
    - otherwise, commits on the same day as closed or none if updated after closing.

--- sources/github.js
+++ sources/github.js
@@ -1,3 +1,4 @@
+const { startOfDay } = require('date-fns')
 const { Octokit } = require('@octokit/core')
 
 const { GITHUB_TOKEN, GITHUB_ORG = 'EQWorks' } = process.env
@@ -40,31 +41,71 @@ module.exports.commitsByRange = searchByRange({
   },
 })
 
-const getIssuesComments = ({
+const updatedWithin = ({ start, end }) => (v) => (Number(new Date(v.created_at)) >= Number(new Date(start))) && (Number(new Date(v.updated_at)) <= Number(new Date(end)))
+
+const getIssueEnrichment = (field) => ({
   issues,
-  start: since,
+  start,
   end,
-}) => Promise.all(issues.filter((v) => v.comments).map((v) => client.request({
-  url: v.comments_url,
-  method: 'GET',
-  since, // matching issue search start, plus the below hack to emulate "before"
-}).then(({ data }) => data.filter((v) => +(new Date(v.updated_at) <= +(new Date(end))))))).then((data) => data.flat())
+}) => Promise.all(issues.filter((v) => v.comments).map(
+  (v) => client.request({
+    url: v[field],
+    method: 'GET',
+    per_page: 100, // let this be max
+    since: start, // matching issue search start, plus the below hack to emulate "in range"
+  }).then(({ data }) => data.filter(updatedWithin({ start, end })))
+)).then((data) => data.flat())
+
+const getIssuesComments = getIssueEnrichment('comments_url')
+
+const getPRsReviews = getIssueEnrichment('review_comments_url')
+
+const getPRsCommits = (prs) => Promise.all(prs.filter((pr) => pr.comments).map(
+  (pr) => client.request({
+    url: pr.commits_url,
+    method: 'GET',
+    per_page: 100, // let this be max
+  }).then(({ data }) => data.filter((r) => {
+    // PR times
+    const updated = startOfDay(new Date(pr.updated_at))
+    const created = startOfDay(new Date(pr.created_at))
+    // commit time
+    const committed = startOfDay(new Date(r.commit.committer.date))
+    // if not closed
+    if (pr.state !== 'closed') {
+      return [created, committed].map(Number).includes(Number(updated))
+    }
+    const closed = startOfDay(new Date(pr.closed_at))
+    return Number(committed) === Number(closed) && Number(closed) >= Number(updated)
+  }).map((r) => ({ ...r, pull_request_url: pr.pull_request_url })))
+)).then((data) => data.flat())
 
 const isPR = (v) => Object.keys(v).includes('pull_request')
 
 module.exports.enrichIssues = async ({ issues, start, end }) => {
   // enrich all (issues and PRs) with issue-level comments
   const comments = await getIssuesComments({ issues, start, end })
-  const commentEnriched = issues.map((issue) => ({
+  const enrichedIssues = issues.map((issue) => ({
     ...issue,
     enriched_comments: comments.filter((v) => v.issue_url === issue.url),
   }))
   // split out pure issues and PRs
-  const prs = commentEnriched.filter(isPR)
-  // enrich PRs with review comments
-  // enrich PRs with commit updates
+  const prs = enrichedIssues.filter(isPR).map((pr) => ({
+    ...pr,
+    pull_request_url: pr.url.replace('/issues/', '/pulls/'),
+    commits_url: `${pr.url.replace('/issues/', '/pulls/')}/commits`,
+    review_comments_url: pr.comments_url.replace('/issues/', '/pulls/'),
+  }))
+  // enrich PRs with commits and review comments
+  const reviews = await getPRsReviews({ issues: prs, start, end })
+  const commits = await getPRsCommits(prs)
+  const enrichedPRs = prs.map((pr) => ({
+    ...pr,
+    enriched_reviews: reviews.filter((v) => v.pull_request_url === pr.pull_request_url),
+    enriched_commits: commits.filter((v) => v.pull_request_url === pr.pull_request_url),
+  }))
   return {
-    issues: commentEnriched.filter((v) => !isPR(v)),
-    prs,
+    issues: enrichedIssues.filter((v) => !isPR(v)),
+    prs: enrichedPRs,
   }
 }
