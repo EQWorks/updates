4f4e31a5cf4e23ae8f83f5814dae403678766186
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2020-12-23 13:15:23 -0500

    github - fix both daily and weekly lack of error catch

--- daily.js
+++ daily.js
@@ -24,7 +24,7 @@ const dailyPreviously = () => {
     .then(formatPreviously)
     .then(uploadMD())
     .then(console.log)
-    .then(console.error)
+    .catch(console.error)
 }
 
 if (require.main === module) {
