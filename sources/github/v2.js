ad3fb73740bd2d611bdad8c511e9d59afc4ef771
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2022-08-05 18:28:15 -0400

    github - split out GraphQL sourced issues and PRs, dedupe linked issues

---
+++ sources/github/v2.js
@@ -0,0 +1,10 @@
+// issues contain both issues and PRs, through __typename
+module.exports.splitIssuesPRs = (data) => {
+  // separate out PRs
+  const prs = data.filter(pr => pr.__typename === 'PullRequest')
+  // separate out issues without PR links
+  const linkedIssueIDs = prs.map(pr => pr.closingIssuesReferences.nodes.map(node => node.id)).flat()
+  const issues = data.filter(issue => issue.__typename === 'Issue' && !linkedIssueIDs.includes(issue.id))
+
+  return { prs, issues }
+}
