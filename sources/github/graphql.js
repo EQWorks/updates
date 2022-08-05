edce7417edc179d5f2058cfdc592c03a582bed40
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2022-08-05 18:26:15 -0400

    github - obtain full issue ref for linked PRs through GraphQL

--- sources/github/graphql.js
+++ sources/github/graphql.js
@@ -3,7 +3,8 @@ const { Octokit } = require('@octokit/core')
 const { GITHUB_TOKEN, GITHUB_ORG = 'EQWorks' } = process.env
 const octokit = new Octokit({ auth: GITHUB_TOKEN })
 
-module.exports.updatedIssuesByRange = async ({ start, end, per_page = 100 }) => {
+
+const updatedByRange = ({ query, parameters = {} }) => async ({ start, end, per_page = 100 }) => {
   let data = []
   let hasNextPage = true
   let endCursor = null
@@ -12,170 +13,9 @@ module.exports.updatedIssuesByRange = async ({ start, end, per_page = 100 }) => 
   while (hasNextPage) {
     // query based on https://docs.github.com/en/search-github
     const { search: { nodes = [], pageInfo = {} } = {} } = await octokit.graphql(
-      `
-        query IssuesQuery($q: String!, $first: Int!, $after: String) {
-          search(
-            query: $q
-            type: ISSUE
-            first: $first # max 100
-            after: $after
-          ) {
-            issueCount # for debugging
-            nodes {
-              __typename
-              ... on Issue {
-                id
-                author {
-                  ... on User {
-                    login
-                    name
-                  }
-                }
-                assignees(first: 100) {
-                  nodes {
-                    login
-                    name
-                    id
-                  }
-                }
-                updatedAt
-                url
-                title
-                state
-                repository {
-                  name
-                  url
-                  repositoryTopics(first: 100) {
-                    nodes {
-                      topic {
-                        name
-                      }
-                    }
-                  }
-                }
-                body
-                closed
-                closedAt
-                createdAt
-                publishedAt
-                updatedAt
-                comments(orderBy: {field: UPDATED_AT, direction: DESC}, first: 100) {
-                  totalCount
-                  nodes {
-                    author {
-                      ... on User {
-                        login
-                        name
-                      }
-                    }
-                    url
-                    updatedAt
-                    createdAt
-                  }
-                }
-                projectsV2(first: 100) {
-                  nodes {
-                    number
-                    url
-                    title
-                  }
-                  totalCount
-                }
-              }
-              ... on PullRequest {
-                id
-                author {
-                  ... on User {
-                    login
-                    name
-                  }
-                }
-                assignees(first: 100) {
-                  nodes {
-                    login
-                    name
-                  }
-                }
-                updatedAt
-                url
-                title
-                state
-                reviews(first: 100) {
-                  totalCount
-                  nodes {
-                    author {
-                      ... on User {
-                        name
-                        login
-                      }
-                    }
-                    url
-                    state
-                    updatedAt
-                    createdAt
-                  }
-                }
-                repository {
-                  name
-                  url
-                  repositoryTopics(first: 100) {
-                    nodes {
-                      topic {
-                        name
-                      }
-                    }
-                  }
-                }
-                body
-                closed
-                closedAt
-                createdAt
-                publishedAt
-                updatedAt
-                comments(orderBy: {field: UPDATED_AT, direction: DESC}, first: 100) {
-                  totalCount
-                  nodes {
-                    author {
-                      ... on User {
-                        login
-                        name
-                      }
-                    }
-                    url
-                    updatedAt
-                    createdAt
-                  }
-                }
-                closingIssuesReferences(
-                  first: 100
-                  orderBy: {field: UPDATED_AT, direction: DESC}
-                ) {
-                  totalCount
-                  nodes {
-                    id
-                    url
-                    number
-                    title
-                  }
-                }
-                projectsV2(first: 100) {
-                  nodes {
-                    number
-                    url
-                    title
-                  }
-                  totalCount
-                }
-              }
-            }
-            pageInfo {
-              endCursor
-              hasNextPage
-            }
-          }
-        }
-      `,
+      query,
       {
+        ...parameters,
         q,
         first: per_page,
         after: endCursor,
@@ -187,3 +27,167 @@ module.exports.updatedIssuesByRange = async ({ start, end, per_page = 100 }) => 
   }
   return data
 }
+
+const issueNode = `
+  id
+  author {
+    ... on User {
+      login
+      name
+    }
+  }
+  assignees(first: 100) {
+    nodes {
+      login
+      name
+      id
+    }
+  }
+  updatedAt
+  url
+  title
+  state
+  repository {
+    name
+    url
+    repositoryTopics(first: 100) {
+      nodes {
+        topic {
+          name
+        }
+      }
+    }
+  }
+  body
+  closed
+  closedAt
+  createdAt
+  publishedAt
+  updatedAt
+  comments(orderBy: {field: UPDATED_AT, direction: DESC}, first: 100) {
+    totalCount
+    nodes {
+      author {
+        ... on User {
+          login
+          name
+        }
+      }
+      url
+      updatedAt
+      createdAt
+    }
+  }
+  projectsV2(first: 100) {
+    nodes {
+      number
+      url
+      title
+    }
+    totalCount
+  }
+`
+
+module.exports.updatedIssuesByRange = updatedByRange({
+  query: `
+    query IssuesQuery($q: String!, $first: Int!, $after: String) {
+      search(
+        query: $q
+        type: ISSUE
+        first: $first # max 100
+        after: $after
+      ) {
+        issueCount # for debugging
+        nodes {
+          __typename
+          ... on Issue {${issueNode}}
+          ... on PullRequest {
+            id
+            author {
+              ... on User {
+                login
+                name
+              }
+            }
+            assignees(first: 100) {
+              nodes {
+                login
+                name
+              }
+            }
+            updatedAt
+            url
+            title
+            state
+            reviews(first: 100) {
+              totalCount
+              nodes {
+                author {
+                  ... on User {
+                    name
+                    login
+                  }
+                }
+                url
+                state
+                updatedAt
+                createdAt
+              }
+            }
+            repository {
+              name
+              url
+              repositoryTopics(first: 100) {
+                nodes {
+                  topic {
+                    name
+                  }
+                }
+              }
+            }
+            body
+            isDraft
+            closed
+            closedAt
+            createdAt
+            publishedAt
+            updatedAt
+            comments(orderBy: {field: UPDATED_AT, direction: DESC}, first: 100) {
+              totalCount
+              nodes {
+                author {
+                  ... on User {
+                    login
+                    name
+                  }
+                }
+                url
+                updatedAt
+                createdAt
+              }
+            }
+            closingIssuesReferences(
+              first: 10
+              orderBy: {field: UPDATED_AT, direction: DESC}
+            ) {
+              totalCount
+              nodes {${issueNode}}
+            }
+            projectsV2(first: 100) {
+              nodes {
+                number
+                url
+                title
+              }
+              totalCount
+            }
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  `,
+})
