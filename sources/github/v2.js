3a1f24996e6e077d4a707f19825c907a33af259b
hyx131 <selin.hyx@gmail.com>
2022-08-09 10:04:31 -0400

    github - filter team topics and enrich repos with team/category

--- sources/github/v2.js
+++ sources/github/v2.js
@@ -1,5 +1,7 @@
 const { DateTime } = require('luxon')
 
+const { isTeamTopic } = require('./util')
+
 
 // issues contain both issues and PRs, through __typename
 module.exports.splitIssuesPRs = (data) => {
@@ -12,17 +14,31 @@ module.exports.splitIssuesPRs = (data) => {
   return { prs, issues }
 }
 
-module.exports.filterRepoReleases = ({ repos, start, end }) => {
+const getTeamTopicsCategory = (repositoryTopics) => {
+  const topics = repositoryTopics.nodes.map(({ topic }) => topic?.name)
+  const teamTopics = topics.filter(isTeamTopic)
+  const team = [0, 2].includes(teamTopics.length) ? undefined : teamTopics[0].substring(5)
+  const category = topics.filter((t) => !isTeamTopic(t))[0]
+
+  return { topics, team, category }
+}
+
+module.exports.filterReleasesTeams = ({ team, repos, start, end }) => {
   const _start = DateTime.fromISO(start, { zone: 'UTC' }).startOf('day')
   const _end = DateTime.fromISO(end, { zone: 'UTC' }).startOf('day')
+  
+  return repos.map(({ releases, repositoryTopics, ...rest }) => {
+    const ttc = getTeamTopicsCategory(repositoryTopics)
 
-  return repos.map(({ releases, ...rest }) => ({
-    ...rest,
-    releases: releases.nodes
-      .filter(({ publishedAt }) => {
-        const published = DateTime.fromISO(publishedAt, { zone: 'UTC' }).startOf('day')
-        return ((published >= _start) && (published <= _end))
-      })
-      .map(({ tag, ...r }) => ({ ...r, tag: tag.name })),
-  }))
+    return ({
+      ...rest,
+      ...ttc,
+      releases: releases.nodes
+        .filter(({ publishedAt }) => {
+          const published = DateTime.fromISO(publishedAt, { zone: 'UTC' }).startOf('day')
+          return ((published >= _start) && (published <= _end))
+        })
+        .map(({ tag, ...r }) => ({ ...r, tag: tag.name })),
+    })
+  }).filter(({ team: t }) => !team || !t || (t.toLowerCase() === team.toLowerCase()))
 }
