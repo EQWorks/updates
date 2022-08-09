2a883ce8ee75f18bfe5bd94a3a0fe389a9415050
hyx131 <selin.hyx@gmail.com>
2022-08-09 09:59:34 -0400

    github - filter weekly repo releases

--- sources/github/v2.js
+++ sources/github/v2.js
@@ -1,3 +1,6 @@
+const { DateTime } = require('luxon')
+
+
 // issues contain both issues and PRs, through __typename
 module.exports.splitIssuesPRs = (data) => {
   // separate out PRs
@@ -7,4 +10,19 @@ module.exports.splitIssuesPRs = (data) => {
   const issues = data.filter(issue => issue.__typename === 'Issue' && !linkedIssueIDs.includes(issue.id))
 
   return { prs, issues }
+}
+
+module.exports.filterRepoReleases = ({ repos, start, end }) => {
+  const _start = DateTime.fromISO(start, { zone: 'UTC' }).startOf('day')
+  const _end = DateTime.fromISO(end, { zone: 'UTC' }).startOf('day')
+
+  return repos.map(({ releases, ...rest }) => ({
+    ...rest,
+    releases: releases.nodes
+      .filter(({ publishedAt }) => {
+        const published = DateTime.fromISO(publishedAt, { zone: 'UTC' }).startOf('day')
+        return ((published >= _start) && (published <= _end))
+      })
+      .map(({ tag, ...r }) => ({ ...r, tag: tag.name })),
+  }))
 }
